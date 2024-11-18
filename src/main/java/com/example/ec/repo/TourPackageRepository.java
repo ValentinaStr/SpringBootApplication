617f08f7fc81b04277ef8c44d6692d367d934b7b
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-11-18 23:34:42 +0200

    Added interface TourRepository TourPackageRepository

---
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -0,0 +1,5 @@
+package com.example.ec.repo;
+import com.example.ec.Domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+public interface TourPackageRepository extends CrudRepository< TourPackage, String> {
+}
