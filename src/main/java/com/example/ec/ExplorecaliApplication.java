e375a62b5bf24c77cd22ff585c4f6570f5937b29
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-11-19 20:34:03 +0200

    Fix import statements and standardize code formatting
    Updated import statements to include specific classes, ensuring clarity. Set default value for 'importFile' property, and standardized code indentation to enhance readability. Added 'findByName' method in TourPackageRepository for better tour package lookup.

--- src/main/java/com/example/ec/ExplorecaliApplication.java
+++ src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -1,7 +1,9 @@
 package com.example.ec;
 
-import com.example.ec.service.*;
-import com.example.ec.domain.*;
+import com.example.ec.domain.Difficulty;
+import com.example.ec.domain.Region;
+import com.example.ec.service.TourPackageService;
+import com.example.ec.service.TourService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -19,114 +22,114 @@ import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
 @SpringBootApplication
 public class ExplorecaliApplication implements CommandLineRunner {
 
-	@Value("${ec.importfile}")
-	private String importFile;
+    @Value("${ec.importfile:src/main/resources/ExploreCalifornia.json}")
+    private String importFile;
 
-	@Autowired
-	private TourPackageService tourPackageService;
-	@Autowired
-	private TourService tourService;
+    @Autowired
+    private TourPackageService tourPackageService;
+    @Autowired
+    private TourService tourService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExplorecaliApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExplorecaliApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		createTourPackages();
-		long numOfTourPackages = tourPackageService.total();
-		createTours(importFile);
-		long numOfTours = tourService.total();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        createTourPackages();
+        long numOfTourPackages = tourPackageService.total();
+        createTours(importFile);
+        long numOfTours = tourService.total();
+    }
 
-	/**
-	 * Initialize all the known tour packages
-	 */
-	private void createTourPackages() {
-		tourPackageService.createTourPackage("BC", "Backpack Cal");
-		tourPackageService.createTourPackage("CC", "California Calm");
-		tourPackageService.createTourPackage("CH", "California Hot springs");
-		tourPackageService.createTourPackage("CY", "Cycle California");
-		tourPackageService.createTourPackage("DS", "From Desert to Sea");
-		tourPackageService.createTourPackage("KC", "Kids California");
-		tourPackageService.createTourPackage("NW", "Nature Watch");
-		tourPackageService.createTourPackage("SC", "Snowboard Cali");
-		tourPackageService.createTourPackage("TC", "Taste of California");
-	}
+    /**
+     * Initialize all the known tour packages
+     */
+    private void createTourPackages() {
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("CH", "California Hot springs");
+        tourPackageService.createTourPackage("CY", "Cycle California");
+        tourPackageService.createTourPackage("DS", "From Desert to Sea");
+        tourPackageService.createTourPackage("KC", "Kids California");
+        tourPackageService.createTourPackage("NW", "Nature Watch");
+        tourPackageService.createTourPackage("SC", "Snowboard Cali");
+        tourPackageService.createTourPackage("TC", "Taste of California");
+    }
 
-	/**
-	 * Create tour entities from an external file
-	 */
-	private void createTours(String fileToImport) throws IOException {
-		TourFromFile.read(fileToImport).forEach(importedTour ->
-				tourService.createTour(importedTour.getTitle(),
-						importedTour.getDescription(),
-						importedTour.getBlurb(),
-						importedTour.getPrice(),
-						importedTour.getLength(),
-						importedTour.getBullets(),
-						importedTour.getKeywords(),
-						importedTour.getPackageType(),
-						importedTour.getDifficulty(),
-						importedTour.getRegion()));
-	}
+    /**
+     * Create tour entities from an external file
+     */
+    private void createTours(String fileToImport) throws IOException {
+        TourFromFile.read(fileToImport).forEach(importedTour ->
+                tourService.createTour(importedTour.getTitle(),
+                        importedTour.getDescription(),
+                        importedTour.getBlurb(),
+                        importedTour.getPrice(),
+                        importedTour.getLength(),
+                        importedTour.getBullets(),
+                        importedTour.getKeywords(),
+                        importedTour.getPackageType(),
+                        importedTour.getDifficulty(),
+                        importedTour.getRegion()));
+    }
 
-	/**
-	 * Helper class to import ExploreCalifornia.json
-	 */
-	private static class TourFromFile {
-		//fields
-		private String packageType, title, description, blurb, price, length,
-				bullets, keywords, difficulty, region;
+    /**
+     * Helper class to import ExploreCalifornia.json
+     */
+    private static class TourFromFile {
+        //fields
+        private String packageType, title, description, blurb, price, length,
+                bullets, keywords, difficulty, region;
 
-		//reader
-		static List<TourFromFile> read(String fileToImport) throws IOException {
-			return new ObjectMapper().setVisibility(FIELD, ANY).
-					readValue(new FileInputStream(fileToImport), new TypeReference<List<TourFromFile>>() {
-					});
-		}
+        //reader
+        static List<TourFromFile> read(String fileToImport) throws IOException {
+            return new ObjectMapper().setVisibility(FIELD, ANY).
+                    readValue(new FileInputStream(fileToImport), new TypeReference<List<TourFromFile>>() {
+                    });
+        }
 
-		protected TourFromFile() {
-		}
+        protected TourFromFile() {
+        }
 
-		String getPackageType() {
-			return packageType;
-		}
+        String getPackageType() {
+            return packageType;
+        }
 
-		String getTitle() {
-			return title;
-		}
+        String getTitle() {
+            return title;
+        }
 
-		String getDescription() {
-			return description;
-		}
+        String getDescription() {
+            return description;
+        }
 
-		String getBlurb() {
-			return blurb;
-		}
+        String getBlurb() {
+            return blurb;
+        }
 
-		Integer getPrice() {
-			return Integer.parseInt(price);
-		}
+        Integer getPrice() {
+            return Integer.parseInt(price);
+        }
 
-		String getLength() {
-			return length;
-		}
+        String getLength() {
+            return length;
+        }
 
-		String getBullets() {
-			return bullets;
-		}
+        String getBullets() {
+            return bullets;
+        }
 
-		String getKeywords() {
-			return keywords;
-		}
+        String getKeywords() {
+            return keywords;
+        }
 
-		Difficulty getDifficulty() {
-			return Difficulty.valueOf(difficulty);
-		}
+        Difficulty getDifficulty() {
+            return Difficulty.valueOf(difficulty);
+        }
 
-		Region getRegion() {
-			return Region.findByLabel(region);
-		}
-	}
+        Region getRegion() {
+            return Region.findByLabel(region);
+        }
+    }
 }
